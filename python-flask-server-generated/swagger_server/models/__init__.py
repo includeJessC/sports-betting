aa5877ee1e3e6bb2909c98e231888cd4d04d10f6
Bogacheva Anna <aabogacheva@yandex-team.ru>
2023-02-25 18:30:02 +0300

    123

---
+++ python-flask-server-generated/swagger_server/models/__init__.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.base_user_info import BaseUserInfo
+from swagger_server.models.bets import Bets
+from swagger_server.models.bets_result import BetsResult
+from swagger_server.models.competition import Competition
+from swagger_server.models.create_bet_body import CreateBetBody
+from swagger_server.models.create_competition_body import CreateCompetitionBody
+from swagger_server.models.create_match_body import CreateMatchBody
+from swagger_server.models.error_response import ErrorResponse
+from swagger_server.models.inline_response200 import InlineResponse200
+from swagger_server.models.match import Match
+from swagger_server.models.register_approve import RegisterApprove
+from swagger_server.models.user_info import UserInfo
+from swagger_server.models.user_meta import UserMeta
