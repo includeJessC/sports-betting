0f4fa55d5d6c198d9012351192aec51a0c7a63ab
Anna Bogacheva <annab070703@gmail.com>
2023-04-27 23:44:11 +0300

    123

--- bot.py
+++ bot.py
@@ -4,10 +4,10 @@ import telebot
 
 import psycopg2
 import uuid
+from cryptography.fernet import Fernet
 
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
 BOT = telebot.TeleBot(TOKEN, parse_mode=None)
-
 
 class DataBaseManagemantSystemBot:
     def __init__(self):
@@ -40,13 +40,19 @@ class DataBaseManagemantSystemBot:
         self.con.commit()
         return code
 
+    def update_password(self, name, password):
+        cur = self.con.cursor()
+        request = f"UPDATE sport_betting.private_users_info SET password = '{password}' WHERE id='{name}'"
+        cur.execute(request)
+        self.con.commit()
+
 @BOT.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     """
     info about commands
     """
     BOT.reply_to(message,
-                 'Мои команды: \n /get_code - выдаст код для регистрации ')
+                 'Мои команды: \n /get_code - выдаст код для регистрации, get_password - поможет восстановить пароль')
 
 @BOT.message_handler(commands=['get_code'])
 def send_code(message):
@@ -64,5 +70,26 @@ def send_code(message):
         BOT.reply_to(message,
                      f'Что-то пошло нет: проверьте правильность введенного ника на сайте')
 
+
+def get_password(message):
+    db = DataBaseManagemantSystemBot()
+    command = message.text
+    db.update_password(message.from_user.username, command)
+    BOT.reply_to(message, 'Ваш пароль обновлен')
+
+
+@BOT.message_handler(commands=['get_password'])
+def send_password(message):
+    """
+    gives the code to registration or error
+    """
+    db = DataBaseManagemantSystemBot()
+    try:
+      if db.check_registered(message.from_user.username):
+        BOT.reply_to(message, f'Придумайте новый пароль: ')
+        BOT.register_next_step_handler(message, get_password)
+    except Exception:
+        BOT.reply_to(message,
+                     f'Что-то пошло нет: проверьте правильность введенного ника на сайте')
 if __name__ == '__main__':
     BOT.infinity_polling()
