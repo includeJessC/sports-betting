09438e20e6d10009e533e0e96c6311239b7f84be
Anna Bogacheva <annab070703@gmail.com>
2023-05-02 00:15:06 +0300

    123

--- python-flask-server-generated/swagger_server/controllers/parser.py
+++ python-flask-server-generated/swagger_server/controllers/parser.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 import requests
@@ -26,8 +27,39 @@ def manager(response):
             id = game.get("~AA")
             parsing_ref = f'https://www.flashscorekz.com/match/{id}/#/match-summary'
             matches.append(
-                {'id': id, 'name': f'{team_1} vs {team_2}', 'start_time': date_start, 'end_time': date_end, 'team1_name': team_1, 'team2_name': team_2,
-                 'team1_res': game.get("AG"), 'team2_res': game.get("AH"), 'parsing_ref': parsing_ref, 'is_active': (date_start > datetime.now())})
+                {'id': id, 'name': f'{team_1} vs {team_2}', 'start_time': date_start, 'end_time': date_end,
+                 'team1_name': team_1, 'team2_name': team_2,
+                 'team1_res': game.get("AG"), 'team2_res': game.get("AH"), 'parsing_ref': parsing_ref,
+                 'is_active': (date_start > datetime.now())})
+    return matches
+
+
+def manager(response, match_id):
+    data = response.text.split('¬')
+    data_list = [{}]
+    for item in data:
+        key = item.split('÷')[0]
+        value = item.split('÷')[-1]
+        if '~' in key:
+            data_list.append({key: value})
+        else:
+            data_list[-1].update({key: value})
+    matches = []
+    for game in data_list:
+        if 'AA' in list(game.keys())[0]:
+            date_start = datetime.fromtimestamp(int(game.get("AD", 0)))
+            date_end = datetime.fromtimestamp(int(game.get("AO", 0))) if game.get("AO", 0) != 0 else None
+            team_1 = game.get("AE")
+            team_2 = game.get("AF")
+            id = game.get("~AA")
+            if id != match_id:
+                continue
+            parsing_ref = f'https://www.flashscorekz.com/match/{id}/#/match-summary'
+            matches.append(
+                {'id': id, 'name': f'{team_1} vs {team_2}', 'start_time': date_start, 'end_time': date_end,
+                 'team1_name': team_1, 'team2_name': team_2,
+                 'team1_res': game.get("AG"), 'team2_res': game.get("AH"), 'parsing_ref': parsing_ref,
+                 'is_active': (date_start > datetime.now())})
     return matches
 
 
@@ -46,7 +78,24 @@ def parse_competition(url):
     finished_matches = manager(response_finished)
     not_finished_matches = manager(response_not_finished)
     return {'competition_id': competition_id, "name": name, 'is_active': True if len(not_finished_matches) else False,
-           'parsing_ref': url, 'ended_matches': finished_matches, 'not_ended_matches': not_finished_matches}
+            'parsing_ref': url, 'ended_matches': finished_matches, 'not_ended_matches': not_finished_matches}
+
+
+def parse_competition_with_special_match(url, match_id):
+    end = url.find('/table') if url.find('/table') != -1 else url.find('/live')
+    competition_id = url[(url.find('#')) + 2:end]
+    response_result = requests.get(url=url, headers=headers)
+    bp = BeautifulSoup(response_result.text, 'lxml')
+    title = bp.find(attrs={'property': 'og:title'})
+    name = title.get('content')
+    url = url[:(url.find('#'))]
+    url_not_finished_matches = url + 'fixtures/'
+    url_finished_matches = url + 'results/'
+    response_not_finished = requests.get(url=url_not_finished_matches, headers=headers)
+    response_finished = requests.get(url=url_finished_matches, headers=headers)
+    finished_match = manager(response_finished, match_id)
+    not_finished_match = manager(response_not_finished, match_id)
+    return {'match': finished_match if finished_match is not None else not_finished_match}
 
 
 def del_blanks(string):
@@ -60,7 +109,8 @@ def del_blanks(string):
 
 def parse_match(url):
     response_match = requests.get(url=url, headers=headers)
-    print(response_match.text)
+    id_ = url[url.find('/match/') + 7:url.find('/#')]
+    print(id_)
     data = response_match.text.split('¬')
     data_list = [{}]
     for item in data:
@@ -71,12 +121,13 @@ def parse_match(url):
         else:
             data_list[-1].update({key: value})
     bp = BeautifulSoup(response_match.text, 'lxml')
-    print(bp)
-    title = bp.find(attrs={'name': 'og:title'})
-    res = title.get('content')
-    score = ''
-    if res[-1].isnumeric() and res[-3].isnumeric():
-        score = f'{res[-3]} : {res[-1]}'
-    first_team = del_blanks(res[:(res.find(' - '))])
-    second_team = del_blanks(res[(res.find(' - ')) + 3:-3])
-    print(first_team, second_team, score)
+    script = bp.find_all('script')
+    competition_id = (
+        json.loads(script[3].text[script[3].text.find('window.environment = ') + 21:].replace(';\n', ''))[
+            'stats2_config'][
+            'tournamentStage'])
+    competition_url_id = (
+        json.loads(script[3].text[script[3].text.find('window.environment = ') + 21:].replace(';\n', ''))['header'][
+            'tournament']['link'])
+    competition_url = 'https://www.flashscorekz.com' + competition_url_id + '#/' + competition_id
+    return parse_competition_with_special_match(competition_url, id_)
