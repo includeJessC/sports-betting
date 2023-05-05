303cdff9efde80bb837baabef3cb7fa58c657398
Anna Bogacheva <annab070703@gmail.com>
2023-05-06 00:29:50 +0300

    123

---
+++ front-sport-betting/src/components/App/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {useNavigate} from "react-router-dom"
+
+function App() {
+    const navigate = useNavigate();
+    const handleLogin = () => {
+        navigate('/login')
+    }
+    const handleRegister = () => {
+        navigate('/register')
+    }
+  return (// !localStorage.getItem('token') || !localStorage.getItem('id') ?
+          <div className="e3_64">
+              <div className="e3_65"></div>
+              <span className="e3_66">Ставки</span>
+              <button className="e3_67"><span className="e3_68" onClick={handleRegister}>Регистрация</span></button>
+              <button className="e3_69"><span className="e3_70" onClick={handleLogin}>Вход</span></button><span className="e3_71">Данное приложение является программным
+      проектом студентки третьего курса образовательной программы “Прикладная математика и информатика” НИУ ВШЭ
+      Богачевой Анны Андреевны</span>
+          </div>
+  );
+}
+
+export default App;
---
+++ front-sport-betting/src/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+// @ts-ignore
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './components/App/App.js';
+import Login from "./components/Login/Login.js";
+import Register from "./components/Register/Register.js";
+import RegisterApprove from "./components/RegisterApprove/RegisterApprove.js";
+import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    },
+    {
+        path: '/login',
+        element: <Login/>
+    },
+    {
+        path: '/register',
+        element: <Register/>
+    },
+    {
+        path: '/approve',
+        element: <RegisterApprove/>
+    }
+]
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
