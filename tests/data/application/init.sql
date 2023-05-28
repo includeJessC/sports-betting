cedb0641ac78af9ac34cb42ac627a5f315e07dbd
Anna Bogacheva <annab070703@gmail.com>
2023-05-28 22:34:06 +0300

    123

--- tests/data/application/init.sql
+++ tests/data/application/init.sql
@@ -90,7 +90,9 @@ ALTER TABLE "sport_betting"."competitions_info" ADD FOREIGN KEY ("created_by") R
 
 ALTER TABLE "sport_betting"."matches_info" ADD FOREIGN KEY ("competition_id") REFERENCES "sport_betting"."competitions_info" ("special_id");
 
-INSERT INTO sport_betting.users_info (id, name, surname) VALUES ('lol', '123', '123'), ('pok', '123', '123');
-INSERT INTO sport_betting.private_users_info (id, password, approved) VALUES ('lol', '123', true), ('pok', '123', false);
+INSERT INTO sport_betting.users_info (id, name, surname) VALUES ('lol', '123', '123'), ('pok', '123', '123'), ('kek', '123', '123');
+INSERT INTO sport_betting.private_users_info (id, password, approved) VALUES ('lol', '123', true), ('pok', '123', false), ('kek', '123', true);
 INSERT INTO sport_betting.codes (id, secret_code) VALUES ('pok', 'value');
-INSERT INTO sport_betting.login_token (id, token) VALUES ('lol', 'qw');
+INSERT INTO sport_betting.login_token (id, token) VALUES ('lol', 'qw'), ('kek', 'qw');
+INSERT INTO sport_betting.competitions_info (id, name, start_time, end_time, is_active, parsing_ref, created_by, special_id) VALUES ('1', '1', now(), NULL, true, NULL, 'lol', '1');
+INSERT INTO sport_betting.matches_info (competition_id, id, name, start_time, end_time, first_team_name, second_team_name, first_team_result, second_team_result, parsing_ref, is_active) VALUES ('1', '1', '1',now(), NULL, '1', '2', 0, 1, NULL. false);
--- tests/tests.py
+++ tests/tests.py
@@ -25,6 +25,7 @@ def test_get_user_info():
     response = requests.get("http://192.168.65.4:8080/user?id=lol")
     print(response.text)
     assert response.status_code == 200
+    assert response.json() == {"id": "lol", "user_meta": {"name": "123", "surname": "123", "password": "123"}}
 
 
 def test_get_user_info_bad_path():
@@ -64,3 +65,70 @@ def test_proxy_authrorized():
                             headers={'X-Token': 'qw', 'X-Username': 'lol'})
     print(response.text)
     assert response.status_code == 200
+
+
+def test_competitions_get():
+    response = requests.get("http://192.168.65.4:8080/competitions?id=lol")
+    assert response.status_code == 200
+    del response.json()['competitions'][0]['matches'][0]['start_time']
+    assert response.json() == {
+        "competitions": [{"name": "1", "id": "1", "is_active": True, "created_by": "lol", "matches": [
+            {"first_team_name": "1", "second_team_name": "2", "id": "1", "name": "1", "is_active": False}]}]}
+
+
+def test_competition_info_get():
+    response = requests.get("http://192.168.65.4:8080/competitions_info?id=lol&competition_id=1")
+    assert response.status_code == 200
+    del response.json()['matches'][0]['start_time']
+    assert response.json() == {"name": "1", "id": "1", "is_active": True, "created_by": "lol", "matches": [
+        {"first_team_name": "1", "second_team_name": "2", "id": "1", "name": "1", "is_active": False}]}
+
+
+def test_competition_info_post():
+    response = requests.post("http://192.168.65.4:8080/competitions_info?id=kek&competition_id=1")
+    assert response.status_code == 200
+    del response.json()['matches'][0]['start_time']
+    assert response.json() == {"name": "1", "id": "1", "is_active": True, "created_by": "lol", "matches": [
+        {"first_team_name": "1", "second_team_name": "2", "id": "1", "name": "1", "is_active": False}]}
+
+
+def test_match_info_get():
+    response = requests.get("http://192.168.65.4:8080/match_info?id=lol&competition_id=1&match_id=1")
+    assert response.status_code == 200
+    assert response.json() == {"first_team_name": "1", "second_team_name": "2", "id": "1", "name": "1",
+                               "is_active": False}
+
+
+def test_create_competition_and_match():
+    response = requests.post("http://192.168.65.4:8080/create/competition?id=lol", json={"name": "2"})
+    assert response.status_code == 200
+    del response.json()['matches'][0]['start_time']
+    c_id = response.json()['id']
+    del response.json()['id']
+    assert response.json() == {"name": "2", "is_active": True, "created_by": "lol", "matches": []}
+    response = requests.post(f"http://192.168.65.4:8080/create/match?id=lol&competition_id={c_id}",
+                             json={"first_team_name": "1", "second_team_name": "2", "special_bets": ["1", "2"]})
+    assert response.status_code == 200
+    response = requests.get(f"http://192.168.65.4:8080/competitions_info?id=lol&competition_id={c_id}")
+    assert response.status_code == 200
+    assert len(response.json()['matches']) != 0
+
+
+def test_competitions_get_bad_path():
+    response = requests.get("http://192.168.65.4:8080/competitions?id=lol1")
+    assert response.status_code == 404
+
+
+def test_competition_info_get_bad_path():
+    response = requests.get("http://192.168.65.4:8080/competitions_info?id=lol&competition_id=12")
+    assert response.status_code == 404
+
+
+def test_competition_info_post_bad_path():
+    response = requests.post("http://192.168.65.4:8080/competitions_info?id=kek&competition_id=12")
+    assert response.status_code == 404
+
+
+def test_match_info_get_bad_path():
+    response = requests.get("http://192.168.65.4:8080/match_info?id=lol&competition_id=1&match_id=12")
+    assert response.status_code == 404
